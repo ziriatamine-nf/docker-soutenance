ebedf0107ce8aa94b162cd2b96c591bef6d31275
ziriatamine-nf <ziriat.amine@gmail.com>
2026-06-12 07:03:37 +0200

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// Configuration Vite minimale pour Vue 3.
-// "npm run build" génère le site statique optimisé dans le dossier /dist,
-// qui sera ensuite servi par Nginx (voir front/Dockerfile, stage 2).
+
 export default defineConfig({
   plugins: [vue()],
 });
